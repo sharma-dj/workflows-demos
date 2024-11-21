00a09fb3409a2c9aa640ab74905f700683e05232
Dhananjay Sharma <dj4388@gmail.com>
2024-11-21 11:18:45 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-echo "$VAULT_PASS" > ~/.vault_pass.txt
-mkdir ~/.ssh
-ansible-vault view --vault-password-file=~/.vault_pass.txt ansible/ssh_key.txt.secret > ~/.ssh/id_rsa
-chmod 0600 ~/.ssh/id_rsa
-
-ansible-playbook -e "build_sha=$GITHUB_SHA" --vault-password-file ~/.vault_pass.txt -i ansible/hosts ansible/deploy.yml
+ansible-vault encrypt_string --vault-password-file ~/.vault_pass.txt 'MySecret' --name 'the_secret'
